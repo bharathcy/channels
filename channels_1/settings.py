b032ce1eade739735670e16cb5067f1a6386367f
bharathcy <bharathcy@gmail.com>
2021-12-29 22:07:35 +0530

    Update settings.py

--- channels_1/settings.py
+++ channels_1/settings.py
@@ -129,7 +129,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [*],
+            "hosts": ['*'],
         },
     },
 }
